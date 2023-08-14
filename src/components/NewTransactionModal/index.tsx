f308d64862a72e6ff3439201d13aa7b7a5072039
Italo Leal <itlcoorporativo@gmail.com>
2023-08-13 22:05:08 -0300

    Criação do componente NewModalTransaction

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,31 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { CloseButton, Content, Overlay } from './styles';
+import { X } from '@phosphor-icons/react';
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+
+      <Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
+
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
+        <form >
+          <input type="text" placeholder="Descrição" required></input>
+          <input type="number" placeholder="Preço" required></input>
+          <input type="text" placeholder="Categoria" required></input>
+
+          <button type='submit'>
+            Cadastrar
+          </button>
+        </form>
+
+      </Content>
+
+    </Dialog.Portal>
+  )
+}
